32e6dccc62ae192d124af8f78ebce51da6f1fd6d
Felipe Souza <felipelsouza.dev@gmail.com>
2021-09-26 19:53:33 -0300

    Fix lint

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { Text, View, TextInput, Image } from "react-native";
-import { RectButton } from "react-native-gesture-handler";
-import google from "../../../../assets/google.png";
-import facebook from "../../../../assets/facebook.png";
-import linkedin from "../../../../assets/linkedin.png";
-import { useNavigation } from "@react-navigation/core";
+import React, { useState } from 'react';
+import { Text, View, TextInput, Image } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import google from '../../../../assets/google.png';
+import facebook from '../../../../assets/facebook.png';
+import linkedin from '../../../../assets/linkedin.png';
+import { useNavigation } from '@react-navigation/core';
 
-import styles from "./styles";
+import styles from './styles';
 
 export const Login = () => {
   const [email, setEmail] = useState('');
@@ -14,7 +14,7 @@ export const Login = () => {
   const navigation = useNavigation();
 
   const handleRegister = () => {
-    navigation.navigate('Register');
+    navigation.navigate('Register' as never);
   };
 
   return (
@@ -42,7 +42,9 @@ export const Login = () => {
           <RectButton style={styles.searchClassButton}>
             <Text style={styles.buttonText}>Entrar</Text>
           </RectButton>
-          <Text onPress={handleRegister} style={styles.p}>Cadastre-se</Text>
+          <Text onPress={handleRegister} style={styles.p}>
+            Cadastre-se
+          </Text>
           <View style={styles.images}>
             <Image source={google} />
             <Image source={facebook} />
